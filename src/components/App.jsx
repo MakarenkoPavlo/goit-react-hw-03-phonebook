728730540c677d8604ab45a948934c04e9571bb7
Makarenko Pavlo <cmertnik1986@gmail.com>
2023-08-07 22:51:59 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
